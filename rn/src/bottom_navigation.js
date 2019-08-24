60928a4b0992a5e5827be91729b0057aeb363a68
Michael Garas <michael.luka@hotmail.com>
2019-08-24 11:02:11 -0400

    Fixed Navigation

--- rn/src/bottom_navigation.js
+++ rn/src/bottom_navigation.js
@@ -2,6 +2,8 @@ import { createStackNavigator, createAppContainer, createMaterialTopTabNavigator
 import Home from './components/pages/home/Home.js';
 import Register from './components/pages/register/Register.js';
 import Login from './components/pages/login/Login.js';
+import Explore from './components/pages/explore/Explore.js';
+import Table from './components/pages/table/Table.js';
 
 
 const BottomAppNavigator = createMaterialTopTabNavigator(
@@ -22,6 +24,18 @@ const BottomAppNavigator = createMaterialTopTabNavigator(
         screen: Login,
         navigationOptions: {
           title: "Login"
+        }
+      },
+      Explore: {
+        screen: Explore,
+        navigationOptions: {
+          title: "Explore"
+        }
+      },
+      Table: {
+        screen: Table,
+        navigationOptions:{
+          title: "Join Table"
         }
       }
     },
