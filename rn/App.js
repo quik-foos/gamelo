f24e1c8024faa8a23f62f997ef32951ca6e4256d
Fred <fred@corinth.local>
2019-08-24 10:12:46 -0400

    changed font globally

--- rn/App.js
+++ rn/App.js
@@ -8,8 +8,12 @@
 
 import React, { Component } from 'react';
 import AppContainer from './src/navigation';
+import GlobalFont from 'react-native-global-font';
 
 export default class App extends Component {
+  componentDidMount() {
+    GlobalFont.applyGlobal('Rubik-Medium');
+  }
   render() {
     return <AppContainer />;
   }
