4f65ac584a3d30bf08b52c6c92854502d6da8180
Fred <fred@corinth.local>
2019-08-24 18:17:08 -0400

    modal page

---
+++ rn/src/components/ui_elems/ModalPage.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
+
+export default class ModalPage extends Component {
+	render() {
+		return (
+			<View style={styles.modalPage}>
+        {this.props.children}
+			</View>
+		);
+	}
+}
+const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get('window').width;
+const styles = StyleSheet.create({
+	modalPage: {
+    position: 'absolute',
+    backgroundColor:'#FFF',
+		left: 0,
+		top: 0,
+    flex: 1,
+    height: windowHeight,
+    width: windowWidth,
+		zIndex: 10
+	}
+})
