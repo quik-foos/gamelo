e2e9cb1234656b85ca6995e4fd9e86f1c2361de2
Wayne Ferrao <wayne.ferrao93@gmail.com>
2019-08-24 22:51:58 -0400

    Conditional rendering on Profile page: history/summary

--- rn/src/components/pages/profile/Profile.js
+++ rn/src/components/pages/profile/Profile.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
+import React, {  Component } from 'react';
 import {
+  Button,
   StyleSheet,
   Text,
   View,
   Image,
+  SafeAreaView,
   TouchableOpacity,
   ToastAndroid
 } from 'react-native';
 import { BUTTON_COLOR } from '../../../constants';
 import { connect } from 'react-redux';
-import { UserApi } from '../../../api';
+import { UserApi, ResultApi } from '../../../api';
 import { logoutAction } from '../../../actions';
 import ButtonSmall from '../../../components/ui_elems/ButtonSmall'
-import Explore from '../../../components/pages/explore/Explore'
 import { ScrollView } from 'react-native-gesture-handler';
+import MaterialTabs from 'react-native-material-tabs';
 
 class Profile extends Component {
 
@@ -23,22 +25,32 @@ class Profile extends Component {
     username: "", 
     games: [], 
     email: "", 
-    loading: false 
+    loading: false,
+    view: "history",
+    gameHistory: [],
+    gameSummary: [{game: "Catan",played:112,elo:1600},{game: "Catan",played:214,elo:1640},{game: "Monopoly",played:42,elo:1200}]
   }
+
+
+
   async componentDidMount(){
-    console.log("I am here")
     console.log(this.props.user)
     try{
       res = await UserApi.findOne({id: this.props.user})
-      console.log(res)
       currentUser = res.data
-      console.log(currentUser)
       if(currentUser) this.setState( { 
         firstName: currentUser.firstName,
         lastName: currentUser.lastName,
         username: currentUser.username,
         email: currentUser.email 
       })
+    } catch(e){
+      console.log(e.response)
+      ToastAndroid.show("Something went wrong. Please check your internet connection", ToastAndroid.SHORT)
+    }
+    try {
+      gameres = await ResultApi.findAll({player:this.props.user})
+      console.log(gameres)
     } catch(e){
       console.log(e.response)
       ToastAndroid.show("Something went wrong. Please check your internet connection", ToastAndroid.SHORT)
@@ -57,8 +69,45 @@ class Profile extends Component {
     }
   };
 
+  displayHistory = () => {
+    this.setState({
+      view:"history"
+    })
+  }
+  displaySummary = () => {
+    this.setState({
+      view:"summary"
+    })
+  }
+
   render() {
+    const History = 
+      (
+      <View>
+        <Text> Board Game Name </Text>
+      </View>
+      );
+    const EloSummary = 
+      (
+      <View>
+       {this.state.gameSummary.map((game, key)=>{
+         return(
+           <View key={key}>
+             <Text>
+              {game.game}  {game.played} 
+              </Text>
+              <Text>
+              {game.elo}
+             </Text>
+           </View>
+         )
+       })}
+      </View>
+      );
+  
+   
     return (
+      
         <ScrollView style={styles.container}>
             <View style={styles.header}></View>
             <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
@@ -66,11 +115,15 @@ class Profile extends Component {
               <View style={styles.bodyContent}>
                 <Text style={styles.name}>{`${this.state.firstName} ${this.state.lastName}`}</Text>
                 <Text style={styles.info}>{this.state.username}</Text>
-                {/* <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text> */}
+                <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text>
+                <View>
+                  <ButtonSmall text="Game History" onPress={this.displayHistory}/> 
+                  <ButtonSmall text="Game Summary" onPress={this.displaySummary}/> 
+                </View>
                 <ButtonSmall text="Sign Out" onPress={this.signout} /> 
+                { this.state.view === "history" ? History : EloSummary}
               </View>
-          </View>
-          <Explore />
+              </View>
         </ScrollView>
 
     );
